02fc02bb11d2833809a1586eb0f3ec1a09e217f6
Гладкий Даниил(QA) <genius.97@bk.ru>
2025-09-09 18:05:41 +0200

    Сценарии нагрузки в Locust: User, TaskSet и SequentialTaskSet.Практика: Использование TaskSet для HTTP и gRPC API-клиентов

---
+++ locust_get_accounts.py
@@ -0,0 +1,47 @@
+from locust import User, TaskSet, task, between
+
+from clients.http.gateway.users.schema import CreateUserResponseSchema
+from clients.http.gateway.accounts.schema import OpenDepositAccountResponseSchema
+from clients.http.gateway.gateway_task_set import GatewayHTTPTaskSet
+
+
+class GetAccountsTaskSet(GatewayHTTPTaskSet, TaskSet):
+    """
+    Нагрузочный сценарий:
+    1. Создание пользователя
+    2. Открытие депозитного счёта
+    3. Получение списка счетов
+    Задачи выполняются в произвольном порядке.
+    """
+
+    create_user_response: CreateUserResponseSchema | None = None
+    open_deposit_account_response: OpenDepositAccountResponseSchema | None = None
+
+    @task(2)
+    def create_user(self):
+        """Создание нового пользователя"""
+        self.create_user_response = self.users_gateway_client.create_user()
+
+    @task(2)
+    def open_deposit_account(self):
+        """Открытие депозитного счёта (если есть пользователь)"""
+        if not self.create_user_response:
+            return
+        self.open_deposit_account_response = self.accounts_gateway_client.open_deposit_account(
+            user_id=self.create_user_response.user.id
+        )
+
+    @task(6)
+    def get_accounts(self):
+        """Получение списка всех счетов (если есть пользователь)"""
+        if not self.create_user_response:
+            return
+        self.accounts_gateway_client.get_accounts(
+            user_id=self.create_user_response.user.id
+        )
+
+
+class GetAccountsScenarioUser(User):
+    host = "localhost"
+    wait_time = between(1, 3)
+    tasks = [GetAccountsTaskSet]
